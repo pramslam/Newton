1a939bc96a8690b137d613408e8c3b33afed5f37
David Lo <pramslam@gmail.com>
2019-03-02 15:05:50 -0800

    Stream both cameras

--- main.cpp
+++ main.cpp
@@ -4,11 +4,33 @@
 using namespace std;
 using namespace cv;
 
+const int fps = 30;
+
 int main()
 {
-	Mat img = imread("lena.png");
-	namedWindow("image", WINDOW_NORMAL);
-	imshow("image", img);
-	waitKey(0);
-	return 0;
+	// initialize and allocate memory to load the video stream from camera
+	VideoCapture cam_0(0);
+	VideoCapture cam_1(1);
+
+	if (!cam_0.isOpened()) { return -1; }
+
+	if (!cam_1.isOpened()) { return -1; }
+
+	while (true)
+	{
+		// grab and retrieve each frame of the video sequentially
+		Mat frame_0;
+		cam_0 >> frame_0;
+		Mat frame_1;
+		cam_1 >> frame_1;
+
+		imshow("Cam 0", frame_0);
+		imshow("Cam 1", frame_1);
+
+		// wait for keypress to exit
+		int c = waitKey(40);			// wait 40 milliseconds
+		if (27 == char(c)) { break; }	// exit loop if user pressed "esc" key
+	}
+
+	return 1;
 }
