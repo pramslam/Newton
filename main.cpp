0516cf44a14165618368ef2cf4cb5cd508c01269
David Lo <pramslam@gmail.com>
2019-03-08 04:51:59 -0800

    Full Loop
    Streams from two cameras, records, plays back recording, loop playback, restart.
    
    Controls:
    R - Record, Restart
    Esc - Exit Program

--- main.cpp
+++ main.cpp
@@ -4,24 +4,44 @@
 using namespace std;
 using namespace cv;
 
-bool streaming = true;
-bool recording = false;
-bool restart = false;
+bool shutdown = false;
 const int fps = 10;
-int frameCount = 0;
 int recordingTime = 10;			// in seconds
-double playbackCount = 0;
+
+string filename_0 = "output_0.avi";
+string filename_1 = "output_1.avi";
+
+void streamcapture(Mat, Mat);	// streams video and saves into an .avi
+void playback(Mat, Mat);		// plays back saved .avi
 
 int main()
 {
-	// STREAM
+	// create frame
+	Mat frame_0;
+	Mat frame_1;
+	
+	do
+	{
+		streamcapture(frame_0, frame_1);
+		playback(frame_0, frame_1);
+	} while (shutdown == false);
+}
+
+// STREAM CAPTURE
+void streamcapture(Mat frame_0, Mat frame_1)
+{
+	bool recording = false;
+	bool streaming = true;
+	int frameCount = 0;
+	cout << " START STREAMING" << endl;
+
 	// initialize and allocate memory to load the video stream from camera
 	VideoCapture cam_0(0);
 	VideoCapture cam_1(1);
 
 	// check that the camera is open
-	if (!cam_0.isOpened()) { return -1; }
-	if (!cam_1.isOpened()) { return -1; }
+	if (!cam_0.isOpened()) { cout << " ERROR - CAM 1 NOT OPENED" << endl; }
+	if (!cam_1.isOpened()) { cout << " ERROR - CAM 2 NOT OPENED" << endl; }
 
 	// store resolution of the video
 	double frameWidth_0 = cam_0.get(CAP_PROP_FRAME_WIDTH);
@@ -30,25 +50,12 @@ int main()
 	double frameHeight_1 = cam_1.get(CAP_PROP_FRAME_HEIGHT);
 
 	// create video writer
-	VideoWriter output_0("output_0.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(frameWidth_0, frameHeight_0));
-	VideoWriter output_1("output_1.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(frameWidth_1, frameHeight_1));
+	VideoWriter output_0(filename_0, VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(frameWidth_0, frameHeight_0));
+	VideoWriter output_1(filename_1, VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(frameWidth_1, frameHeight_1));
 
-	// create frame
-	Mat frame_0;
-	Mat frame_1;
-
-	while (true)
+	do
 	{
-		// wait for keypress to exit
-		int c = waitKey(40);			// wait 40 milliseconds
-		if (27 == char(c)) { break; }	// exit loop if user pressed "esc" key
-		if (recording == false && (114 == char(c) || 82 == char(c)))	// wait for keypress to record
-		{
-			recording = true;
-			cout << " START RECORDING" << endl;
-		}
-
-		// grab and retrieve each frame of the video sequentially
+		// push image to frame
 		cam_0 >> frame_0;
 		cam_1 >> frame_1;
 
@@ -64,31 +71,49 @@ int main()
 			output_1.write(frame_1);
 
 			// out of time
-			if (frameCount >= (fps * recordingTime)) { break; }
+			if (frameCount >= (fps * recordingTime))
+			{
+				recording = false;
+				streaming = false;
+				cout << " STOP RECORDING" << endl;
+			}
 			frameCount++;
 		}
-	}
+
+		// wait for keypress to exit
+		int c = waitKey(40);							// wait 40 milliseconds
+		if (27 == char(c)) { break; }					// exit loop if user pressed "esc" key
+		if (recording == false && (114 == char(c) || 82 == char(c)))	// wait for keypress to record
+		{
+			recording = true;
+			cout << " START RECORDING" << endl;
+		}
+	} while (streaming == true);
 
 	// clean up
 	output_0.release();
 	output_1.release();
+	cout << " OUTPUT RELEASED" << endl;
+	cam_0.release();
+	cam_1.release();
+	cout << " CAM RELEASED" << endl;
+}
 
-	// PLAYBACK
+// PLAYBACK
+void playback(Mat frame_0, Mat frame_1)
+{
+	double playbackCount = 0;
 	cout << " PLAYBACK START" << endl;
-
-	// filename strings
-	string filename_0 = "output_0.avi";
-	string filename_1 = "output_1.avi";
 
 	// initialize and allocate memory to load the video stream from camera
 	VideoCapture file_0(filename_0);
 	VideoCapture file_1(filename_1);
 
 	// check that the camera is open
-	if (!file_0.isOpened()) { return -1; }
-	if (!file_1.isOpened()) { return -1; }
+	if (!file_0.isOpened()) { cout << " ERROR - FILE 1 NOT OPENED" << endl; }
+	if (!file_1.isOpened()) { cout << " ERROR - FILE 2 NOT OPENED" << endl; }
 
-	while (true)
+	while (playbackCount >= 0)
 	{
 		playbackCount++;
 
@@ -98,7 +123,7 @@ int main()
 			playbackCount = 0;
 			file_0.set(CAP_PROP_POS_AVI_RATIO, 0);
 			file_1.set(CAP_PROP_POS_AVI_RATIO, 0);
-			cout << " VIDEO LOOPED" << endl;
+			cout << " PLAYBACK LOOPED" << endl;
 		}
 
 		// push image to frame
@@ -110,10 +135,11 @@ int main()
 		imshow("Cam 1", frame_1);
 
 		// wait for keypress to exit
-		int c = waitKey(40);			// wait 40 milliseconds
-		if (27 == char(c)) { break;	}	// exit loop if user pressed "esc" key
-		if (restart == false && (114 == char(c) || 82 == char(c)))	// wait for keypress to record
+		int c = waitKey(40);					// wait 40 milliseconds
+		if (27 == char(c)) { break; }			// exit loop if user pressed "esc" key
+		if (114 == char(c) || 82 == char(c))	// wait for keypress to record
 		{
+			playbackCount = -1;
 			cout << " RESTARTING..." << endl;
 		}
 	}
