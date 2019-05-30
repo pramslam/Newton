3909ddccf2888cc45c8406528c7c66b240044492
David Lo <pramslam@gmail.com>
2019-05-29 17:32:54 -0700

    Updated Text
    Changed text color, content and instructions
    
    added .tff font for future

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+// Created by David Lo 2018-2019
+// for
+// Eugene Science Center "Jump" Exhibit
+// Project dates 12/18 - 
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
@@ -6,12 +11,21 @@ using namespace std;
 using namespace cv;
 
 bool shutdown = false;
+bool bRecordingIndicator = true;	// show recording indicator
 const int fps = 10;
-int recordingTime = 10;			// in seconds
-int replayCount = 1;			// # of replays during playback
+int recordingTime = 10;				// in seconds
+int replayCount = 1;				// # of replays during playback
+
+double frameWidth_0;
+double frameHeight_0;
+double frameWidth_1;
+double frameHeight_1;
+
+double frameDisplayWidth;
+double frameDisplayHeight;
 
 string bgName = "Background Window";
-string frameDisplayName = "Display";
+string frameDisplayName = " ";
 string frameName_0 = "Front";
 string frameName_1 = "Side";
 
@@ -19,46 +33,46 @@ string filename_0 = "output_front.avi";
 string filename_1 = "output_side.avi";
 
 int main();
+void StreamCapture(Mat, Mat, Mat);		// streams video and saves into an .avi
+void Playback(Mat, Mat, Mat);			// plays back saved .avi
+void ClearDisplay(Mat);					// clears the display
 
-void streamcapture(Mat, Mat, Mat);	// streams video and saves into an .avi
-void playback(Mat, Mat);		// plays back saved .avi
-void clearDisplay(Mat);			// clears the display
 int main()
 {
 	// create background
 	Mat bg(512, 512, CV_8UC3, Scalar(0));
 	namedWindow(bgName, WINDOW_NORMAL);
 	setWindowProperty(bgName, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN); 
-	putText(bg, "Newton JUMP!", Point(10, 50), FONT_HERSHEY_COMPLEX, 2, Scalar(255, 255, 255), 2, LINE_AA, false);		// Draw Text Title
 	imshow(bgName, bg);
 
 	// create windows and position them
-	Mat frame_display(256, 512, CV_8UC3, Scalar(0));
-	namedWindow(frameDisplayName, WINDOW_NORMAL);
-	resizeWindow(frameDisplayName, 300, 100);
-	moveWindow(frameDisplayName, 515, 615);
-	imshow(frameDisplayName, frame_display);
-
 	Mat frame_0;
 	namedWindow(frameName_0);
-	moveWindow(frameName_0, 0, 100);
+	moveWindow(frameName_0, 0, 200);
 
 	Mat frame_1;
 	namedWindow(frameName_1);
-	moveWindow(frameName_1, 700, 100);
+	moveWindow(frameName_1, 700, 200);
 
+	Mat frame_display(256, 1024, CV_8UC3, Scalar(0));
+	namedWindow(frameDisplayName, WINDOW_NORMAL);
+	moveWindow(frameDisplayName, 0, 0);
+	resizeWindow(frameDisplayName, 1350, 198);			// Manually sized to block upper window tabs of frame_1 & 2
+	imshow(frameDisplayName, frame_display);
+
+	// MAIN LOOP
 	do
 	{
-		clearDisplay(frame_display);
-		streamcapture(frame_0, frame_1, frame_display);
-		playback(frame_0, frame_1);
+		ClearDisplay(frame_display);
+		StreamCapture(frame_0, frame_1, frame_display);
+		Playback(frame_0, frame_1, frame_display);
 	} while (shutdown == false);
 
 	return 0;
 }
 
 // STREAM CAPTURE
-void streamcapture(Mat frame_0, Mat frame_1, Mat frame_display)
+void StreamCapture(Mat frame_0, Mat frame_1, Mat frame_display)
 {
 	bool recording = false;
 	bool streaming = true;
@@ -77,14 +91,22 @@ void streamcapture(Mat frame_0, Mat frame_1, Mat frame_display)
 	if (!cam_1.isOpened()) { cout << " ERROR - CAM 2 NOT OPENED" << endl; }
 
 	// store resolution of the video
-	double frameWidth_0 = cam_0.get(CAP_PROP_FRAME_WIDTH);
-	double frameHeight_0 = cam_0.get(CAP_PROP_FRAME_HEIGHT);
-	double frameWidth_1 = cam_1.get(CAP_PROP_FRAME_WIDTH);
-	double frameHeight_1 = cam_1.get(CAP_PROP_FRAME_HEIGHT);
+	frameWidth_0 = cam_0.get(CAP_PROP_FRAME_WIDTH);
+	frameHeight_0 = cam_0.get(CAP_PROP_FRAME_HEIGHT);
+	frameWidth_1 = cam_1.get(CAP_PROP_FRAME_WIDTH);
+	frameHeight_1 = cam_1.get(CAP_PROP_FRAME_HEIGHT);
+
+	// store display size
+	frameDisplayWidth = frame_display.cols;
+	frameDisplayHeight = frame_display.rows;
 
 	// create video writer
 	VideoWriter output_0(filename_0, VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(frameWidth_0, frameHeight_0));
 	VideoWriter output_1(filename_1, VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(frameWidth_1, frameHeight_1));
+
+	// Intro text
+	putText(frame_display, "PUSH BUTTON TO START", Point(frameDisplayWidth / 9.5, frameDisplayHeight / 1.6), FONT_HERSHEY_COMPLEX, 2, Scalar(255, 0, 0), 3, LINE_AA, false);
+	imshow(frameDisplayName, frame_display);
 
 	do
 	{
@@ -95,45 +117,51 @@ void streamcapture(Mat frame_0, Mat frame_1, Mat frame_display)
 		// display countdown timer
 		if (countdown == true)
 		{
+			// Offsets margin from edge
+			float numOffsetW = 2.2;
+			float numOffsetH = 1.3;
+
 			if (delayCount > 0 && delayCount < fps * 2)					// Get Ready
 			{
-				putText(frame_display, "Get Ready!", Point(70, 130), FONT_HERSHEY_COMPLEX, 2, Scalar(0, 0, 255), 3, LINE_AA, false);
+				frame_display = Scalar(0);
+				putText(frame_display, "READY?", Point(frameDisplayWidth / 2.6, frameDisplayHeight / 1.6), FONT_HERSHEY_COMPLEX, 2, Scalar(255, 0, 0), 3, LINE_AA, false);
 				imshow(frameDisplayName, frame_display);
 			}
 			else if (delayCount > fps * 2 && delayCount < fps * 4)		// 3
 			{
 				countdownTime = 3;
 				frame_display = Scalar(0);
-				putText(frame_display, to_string(countdownTime), Point(220, 160), FONT_HERSHEY_COMPLEX, 5, Scalar(0, 0, 255), 3, LINE_AA, false);
+				putText(frame_display, to_string(countdownTime), Point(frameDisplayWidth / numOffsetW, frameDisplayHeight / numOffsetH), FONT_HERSHEY_COMPLEX, 5, Scalar(255, 0, 0), 3, LINE_AA, false);
 				imshow(frameDisplayName, frame_display);
 			}
 			else if (delayCount > fps * 4 && delayCount < fps * 6)		// 2
 			{
 				countdownTime = 2;
 				frame_display = Scalar(0);
-				putText(frame_display, to_string(countdownTime), Point(220, 160), FONT_HERSHEY_COMPLEX, 5, Scalar(0, 0, 255), 3, LINE_AA, false);
+				putText(frame_display, to_string(countdownTime), Point(frameDisplayWidth / numOffsetW, frameDisplayHeight / numOffsetH), FONT_HERSHEY_COMPLEX, 5, Scalar(255, 0, 0), 3, LINE_AA, false);
 				imshow(frameDisplayName, frame_display);
 			}
 			else if (delayCount > fps * 6 && delayCount < fps * 8)		// 1
 			{
 				countdownTime = 1;
 				frame_display = Scalar(0);
-				putText(frame_display, to_string(countdownTime), Point(220, 160), FONT_HERSHEY_COMPLEX, 5, Scalar(0, 0, 255), 3, LINE_AA, false);
+				putText(frame_display, to_string(countdownTime), Point(frameDisplayWidth / numOffsetW, frameDisplayHeight / numOffsetH), FONT_HERSHEY_COMPLEX, 5, Scalar(255, 0, 0), 3, LINE_AA, false);
 				imshow(frameDisplayName, frame_display);
 			}
 			else if (delayCount > fps * 8 && delayCount < fps * 10)		// JUMP!
 			{
-				// start recording, draw start text
+				// start recording
 				recording = true;
 				countdownTime = 0;
 				frame_display = Scalar(0);
-				putText(frame_display, "JUMP!", Point(25, 170), FONT_HERSHEY_COMPLEX, 5, Scalar(0, 0, 255), 3, LINE_AA, false);
+				putText(frame_display, "JUMP!!!", Point(frameDisplayWidth / 4, frameDisplayHeight / numOffsetH), FONT_HERSHEY_COMPLEX, 5, Scalar(255, 0, 0), 3, LINE_AA, false);
 				imshow(frameDisplayName, frame_display);
 			}
 			else if (delayCount >= fps * 12)
 			{
+				// display newtons from capture here TODO////////////////////
 				frame_display = Scalar(0);
-				putText(frame_display, "600N", Point(20, 170), FONT_HERSHEY_COMPLEX, 5, Scalar(0, 0, 255), 3, LINE_AA, false);
+				//putText(frame_display, "600N", Point(frameDisplayWidth / 3.4, frameDisplayHeight / numOffsetH), FONT_HERSHEY_COMPLEX, 5, Scalar(255, 0, 0), 3, LINE_AA, false);
 				imshow(frameDisplayName, frame_display);
 			
 				// cleanup, reset flags
@@ -146,7 +174,7 @@ void streamcapture(Mat frame_0, Mat frame_1, Mat frame_display)
 		}
 		
 		// draw recording indicator
-		if (recording == true)
+		if (recording == true && bRecordingIndicator == true)
 		{
 			circle(frame_0, Point(20, 20), 3, Scalar(0, 0, 255), 5, LINE_AA, 0);
 			circle(frame_1, Point(20, 20), 3, Scalar(0, 0, 255), 5, LINE_AA, 0);
@@ -194,7 +222,7 @@ void streamcapture(Mat frame_0, Mat frame_1, Mat frame_display)
 }
 
 // PLAYBACK
-void playback(Mat frame_0, Mat frame_1)
+void Playback(Mat frame_0, Mat frame_1, Mat frame_display)
 {
 	if (shutdown == true) { return; }
 	
@@ -212,6 +240,23 @@ void playback(Mat frame_0, Mat frame_1)
 
 	while (playbackCount >= 0)
 	{
+		// Clear frame display
+		frame_display = Scalar(0);
+
+		// Frame display replay text
+		if (playbackLoop == 0)
+		{
+			// Replay text display
+			putText(frame_display, "REPLAY", Point(frameDisplayWidth / 2.6, frameDisplayHeight / 1.6), FONT_HERSHEY_COMPLEX, 2, Scalar(255, 0, 0), 3, LINE_AA, false);
+			imshow(frameDisplayName, frame_display);
+		}
+		else if (playbackLoop >= 1)
+		{
+			// Slow-Mo text display
+			putText(frame_display, "SLOW MOTION REPLAY", Point(frameDisplayWidth / 8, frameDisplayHeight / 1.6), FONT_HERSHEY_COMPLEX, 2, Scalar(255, 0, 0), 3, LINE_AA, false);
+			imshow(frameDisplayName, frame_display);
+		}
+
 		playbackCount++;
 
 		// loop if at end
@@ -262,7 +307,8 @@ void playback(Mat frame_0, Mat frame_1)
 	cout << " FILES RELEASED" << endl;
 }
 
-void clearDisplay(Mat frame_display)
+// clears the display frame
+void ClearDisplay(Mat frame_display)
 {
 	frame_display = Scalar(0);
 	imshow(frameDisplayName, frame_display);
